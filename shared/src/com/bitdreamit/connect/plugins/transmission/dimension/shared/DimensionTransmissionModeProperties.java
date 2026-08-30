4d12c47837a21db9df1885ba16e75a6207aafa28
SirajCse <sirajuddoulla@gmail.com>
2026-08-30 21:44:38 +0600

    updating
      setStartOfMessageBytes(String.format("%02X", DimensionConstants.STX));
            setEndOfMessageBytes(String.format("%02X", DimensionConstants.ETX));

--- shared/src/com/bitdreamit/connect/plugins/transmission/dimension/shared/DimensionTransmissionModeProperties.java
+++ shared/src/com/bitdreamit/connect/plugins/transmission/dimension/shared/DimensionTransmissionModeProperties.java
@@ -83,6 +83,7 @@ public class DimensionTransmissionModeProperties extends FrameModeProperties {
         setStartOfMessageBytes(String.format("%02X", DimensionConstants.STX));
         setEndOfMessageBytes(String.format("%02X", DimensionConstants.ETX));
     }
+
     /**
      * Metadata describing every editable property of this transmission mode
      * (display name, description, editor type, default value).
